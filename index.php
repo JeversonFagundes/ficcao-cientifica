1043e637e06f6fb3be58159da2773ac3785cf1c8
Jeverson Fagundes <jeverson.2022311922@aluno.iffar.edu.br>
2024-11-19 14:16:27 -0300

    coimit

--- index.php
+++ index.php
@@ -13,11 +13,55 @@
 
 <body>
 
-<?php
-require_once "headerNav.php";
-?>
+    <?php
+    require_once "headerNav.php";
+    ?>
 
-  
+    <h1 class="center-align">O que é o gênero ficção cientifica?</h1>
+    <main class="container">
+
+        <div class="card-panel">
+
+            <p>
+                A ficção científica é um gênero literário e cinematográfico que explora temas baseados em avanços tecnológicos, científicos e futuros alternativos, frequentemente projetando cenários futurísticos ou distópicos. Esse gênero usa a imaginação para investigar questões filosóficas, sociais e éticas, envolvendo geralmente os seguintes elementos:
+            </p>
+
+            <p> <strong>Elementos Comuns da Ficção Científica:</strong></p>
+            <p><strong>Tecnologia Avançada:</strong></p>
+            <ul>
+                <li>Robôs, inteligência artificial, viagens espaciais, teletransporte, entre outros.</li>
+            </ul>
+
+            <p><strong>Exploração do Espaço e Viagens no Tempo:</strong></p>
+            <ul>
+                <li> Histórias que se passam em outros planetas, galáxias distantes ou futuras eras da humanidade.</li>
+            </ul>
+
+            <p><strong> Cenários Futurísticos ou Alternativos:</strong></p>
+            <ul>
+                <li>
+                    Mundos distópicos, utópicos ou civilizações alienígenas.
+                </li>
+            </ul>
+
+            <p><strong>Temas Filosóficos e Éticos:</strong></p>
+            <ul>
+                <li>
+                    Questões sobre a natureza da humanidade, ética do uso de tecnologia, evolução da sociedade, etc.
+                </li>
+            </ul>
+
+            <p>
+                <strong> Ciência e Experimentos:</strong>
+            </p>
+            <ul>
+                <li>
+                    Foco em descobertas científicas, experiências genéticas, ou teorias científicas como a relatividade e mecânica quântica.
+                </li>
+            </ul>
+        </div>
+    </main>
+
 </body>
 
 </html>
